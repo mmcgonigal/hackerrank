3be8bf29f4ab0227742e4094dbdb42151fb1b730
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-05-31 02:22:00 -0500

    System.out.printf

--- src/HackerRank.java
+++ src/HackerRank.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class HackerRank {
-
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
@@ -15,23 +14,16 @@ public class HackerRank {
 //        System.out.println("Int:" + i);
 
         System.out.println("================================");
-        for(int i=0;i<3;i++)
-        {
-            String s1=scan.next();
-            int x=scan.nextInt();
-            if( 10<x && x < 100){
-                String num = '0' +Integer.toString(x) ;
-                System.out.printf(s1+"%15s%n",num);
-            } else if ( x <10){
-                String num = "00" + Integer.toString(x);
-                System.out.printf(s1+"%15s%n",num);
-            } else if (100 < x){
-                System.out.printf(s1+ "%15s%n", x);
-            }
-
+        for (int i = 0; i < 3; i++) {
+            String s1 = scan.next();
+            int x = scan.nextInt();
+            System.out.printf("%-15s%03d%n",s1,x);
             //Complete this line
+
         }
         System.out.println("================================");
 
+
     }
+
 }
