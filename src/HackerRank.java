ea4844e648f64e27c8337979b82be16a6fe1bc75
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-05-30 23:54:45 -0500

    solved Scanner , String nextLine()

--- src/HackerRank.java
+++ src/HackerRank.java
@@ -7,8 +7,8 @@ public class HackerRank {
         Scanner scan = new Scanner(System.in);
         int i = scan.nextInt();
         double d= scan.nextDouble();
-
-        String s = scan.next();
+        scan.nextLine();
+        String s = scan.nextLine();
 
         System.out.println("String:" + s);
         System.out.println("Double:" + d);
