e7d1364f8d44e5ebf044e804977391a1731bf81d
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-06-02 23:38:39 -0500

    hackerrank datatype

---
+++ src/Datatypes_solution.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Datatypes_solution {
+
+    public static void main(String [] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+       int t = scanner.nextInt();
+       for (int i=0; i<t; i++){
+           try
+           {
+               long x=scanner.nextLong();
+               System.out.println(x+" can be fitted in:");
+               if(x>=-128 && x<=127)System.out.println("* byte");
+               //Complete the code
+               else if(-32768<=x && x<=32767) System.out.println("* short");
+               else if(-2147483648 <=x && x<=2147483647) System.out.println("* int");
+               else if(-9223372036854775808 <= x && x <=9223372036854775807) {
+;                   System.out.println("* long");
+               }
+           }
+           catch(Exception e)
+           {
+               System.out.println(scanner.next()+" can't be fitted anywhere.");
+           }
+       }
+        
+
+    }
+}
