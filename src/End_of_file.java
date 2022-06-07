2818c19ac6987e43fcd1d5fad8a3722bf622e4c4
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-06-06 23:47:20 -0500

    finished end of file solution

--- src/End_of_file.java
+++ src/End_of_file.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class End_of_file {
     public static void main (String[] str){
+
+        Scanner scanner = new Scanner(System.in);
+        int i = 1;
+        String  a;
+        while(scanner.hasNext()){
+            a = scanner.nextLine();
+            System.out.println( i +" "+ a );
+            i++;
+        }
+
+
 
     }
 }
