0397ddd69870b623078e41eb03b2791b4fdeadd0
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-06-09 00:33:01 -0500

    refactor the solution

--- src/Static_initializer_block.java
+++ src/Static_initializer_block.java
@@ -1,15 +1,24 @@
 import java.util.Scanner;
 
 public class Static_initializer_block {
+        static Scanner scanner = new Scanner(System.in);
+        static int x=scanner.nextInt();
+        static int y= scanner.nextInt();
+        static boolean flag =  true;
+       static {
+           try {
+               if (x <= 0 || y <= 0) {
+                   flag = false;
+                   throw new Exception("Breadth and height must be positive");
+               }
+           } catch (Exception e) {
+               System.out.println(e);
+           }
+       }
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int x=scanner.nextInt();
-        int y= scanner.nextInt();
-        try{
-            if(x <= 0 || y<= 0){
-                throw new Exception("Breadth and height must be positive");
-        }}catch(Exception e){
-            System.out.println(e);
+        if(flag){
+            int area=x*y;
+            System.out.print(area);
         }
 
     }
