97b13e6f2fab673af5068d2849eed6b0e6363ca0
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-06-11 22:50:39 -0500

    currency

---
+++ src/Currency.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Currency {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        // Write your code here.
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
--- src/Date_and_Time.java
+++ src/Date_and_Time.java
@@ -3,19 +3,16 @@ import java.util.Scanner;
 
 public class Date_and_Time {
 
-    public String findDay (int month, int day, int year){
+    public String findDay (int month, int day, int year) {
 
         int mm = Integer.valueOf(month);
         int dd = Integer.valueOf(day);
-        int yyyy= Integer.valueOf(year);
+        int yyyy = Integer.valueOf(year);
 
-        LocalDate localDate = LocalDate.of(yyyy,mm,dd);
+        LocalDate localDate = LocalDate.of(yyyy, mm, dd);
 
-        return localDate.getDateOfWeek.toString;
-
-
-
-
+        //  return localDate.getDateOfWeek.toString();
+        return "";
 
 
     }
