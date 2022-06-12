571125a20a45e20423dbf4f5121f5a91e62696d2
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-06-11 23:10:58 -0500

    refactor

--- src/Currency.java
+++ src/Currency.java
@@ -10,10 +10,16 @@ public class Currency {
         scanner.close();
 
         // Write your code here.
-        NumberFormat us = NumberFormat.getCurrencyInstance(Locale.US);
-        NumberFormat india = NumberFormat.getCurrencyInstance(new Locale("en","IN"));
-        NumberFormat china = NumberFormat.getCurrencyInstance(Locale.CHINA);
-        NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        NumberFormat o_us = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat o_india = NumberFormat.getCurrencyInstance(new Locale("en","IN"));
+        NumberFormat o_china = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat o_france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+
+
+        String us=o_us.format(payment);
+        String india=o_india.format(payment);
+        String china=o_china.format(payment) ;
+        String france=o_france.format(payment);
 
 
         System.out.println("US: " + us);
